8feacac00c7c7d222bba207055a8bb0cb3dea664
jhenry33 <jhenry33@uncc.edu>
2019-10-16 17:26:15 -0400

    Random AI
    Implement a random AI, as well as a enemy movement foundation. Also implemented Walls and the wall tag for collision

---
+++ Namcap/Assets/Scripts/EnemyMovementCore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementCore : MonoBehaviour
+{
+    public float enemySpeed;
+    public void moveRight()
+    {
+        transform.Translate(Vector2.right * enemySpeed * Time.deltaTime);
+    }
+    public void moveLeft()
+    {
+        transform.Translate(Vector2.left * enemySpeed * Time.deltaTime);
+    }
+    public void moveUp()
+    {
+        transform.Translate(Vector2.up * enemySpeed * Time.deltaTime);
+    }
+    public void moveDown()
+    {
+        transform.Translate(Vector2.down * enemySpeed * Time.deltaTime);
+    }
+
+}
---
+++ Namcap/Assets/Scripts/RandomAI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAI : MonoBehaviour
+{
+    int oldDirection;
+    int curDirection;
+    public float accelerationTime = 2f;
+    private float timeLeft;
+    public EnemyMovementCore emc;
+    void getRandom(int direction)
+    {
+        switch (direction)
+        {
+            case 0:
+                emc.moveRight();
+                break;
+            case 1:
+                emc.moveLeft();
+                break;
+            case 2:
+                emc.moveDown();
+                break;
+            case 3:
+                emc.moveUp();
+                break;
+        }
+    }
+    void Start()
+    {
+        curDirection = Random.Range(0, 4);
+        getRandom(curDirection);
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Wall")
+        {
+            oldDirection = curDirection;
+            curDirection = Random.Range(0, 4);
+        }
+        while (curDirection == oldDirection)
+        {
+            curDirection = Random.Range(0, 4);
+        }
+        getRandom(curDirection);
+        Debug.Log(curDirection);
+    }
+    private void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if(timeLeft <= 0)
+        {
+            curDirection = Random.Range(0, 4);
+            timeLeft = accelerationTime;
+        }
+        getRandom(curDirection);
+    }
+}
