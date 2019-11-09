48ebcc6778b30f6059a0029dea98aa2cd57020e8
CMay75 <46727122+CMay75@users.noreply.github.com>
2019-11-09 11:35:09 -0500

    Changed Sprites and Added Exit
    Changed the sprites for the random enemies to be skeletons. Also added a lader for scene transitions and added code to change scenes (WIP).

--- Namcap/Assets/Scripts/PlayerController.cs
+++ Namcap/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,28 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public int keysCollected;
+
+    void Start()
+    {
+
+    }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Key")
         {
             Debug.Log("Collected key!");
+            keysCollected++;
             Destroy(other.gameObject);
+        }
+    
+        if (other.gameObject.tag == "Ladder")
+        {
+            if (keysCollected == 2)
+            {
+                //SceneManager.LoadScene();
+            }
         }
     }
 
