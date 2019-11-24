3def50ab5c3f9d96990e71a14d5680c194dd70ee
jhenry33 <jhenry33@uncc.edu>
2019-11-24 00:01:49 -0500

    Pathfinding Code added
    The Grid works and according to debug tools the pathfinding works. If it doesn't work remove pathfinding from PathManager.

---
+++ Namcap/Assets/Scripts/Grid.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public LayerMask WallMask;//what layer are obstacles?
+    public Vector2 gridWorldSize;//How big grid?
+    public float nodeRadius;//how big node?
+    public float Distance;//how far node from node
+    Node[,] grid;
+    public List<Node> Path;
+    float nodeDiameter;
+    int gridSizeX;
+    int gridSizeY;
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector2 bottomLeft = ((Vector2)transform.position) - Vector2.right * gridWorldSize.x / 2 - Vector2.up * gridWorldSize.y / 2;
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector2 worldPoint = bottomLeft + Vector2.right * (x * nodeDiameter + nodeRadius) + Vector2.up * (y * nodeDiameter + nodeRadius);
+                bool Wall = false;
+                if (Physics2D.OverlapCircle(worldPoint, nodeRadius, WallMask))
+                {
+                    Wall = true;
+                }
+                grid[x, y] = new Node(Wall, worldPoint, x, y);
+            }
+        }
+    }
+    public Node NodeFromWorldPosition(Vector3 a_WorldPosition)
+    {
+        float xpoint = ((a_WorldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x);
+        float ypoint = ((a_WorldPosition.y + gridWorldSize.y / 2) / gridWorldSize.y);
+
+        xpoint = Mathf.Clamp01(xpoint);
+        ypoint = Mathf.Clamp01(ypoint);
+        int x = Mathf.RoundToInt((gridSizeX - 1) * xpoint);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * ypoint);
+
+        return grid[x, y];
+    }
+    public List<Node> GetNeighboringNodes(Node a_NeighborNode)
+    {
+        List<Node> NeighborList = new List<Node>();
+        int checkX;
+        int checkY;
+        checkX = a_NeighborNode.gridX + 1;
+        checkY = a_NeighborNode.gridY;
+        if (checkX >= 0 && checkX < gridSizeX)
+        {
+            if (checkY >= 0 && checkY < gridSizeY)
+            {
+                NeighborList.Add(grid[checkX, checkY]);
+            }
+        }
+        checkX = a_NeighborNode.gridX - 1;
+        checkY = a_NeighborNode.gridY;
+        if (checkX >= 0 && checkX < gridSizeX)
+        {
+            if (checkY >= 0 && checkY < gridSizeY)
+            {
+                NeighborList.Add(grid[checkX, checkY]);
+            }
+        }
+        checkX = a_NeighborNode.gridX;
+        checkY = a_NeighborNode.gridY + 1;
+        if (checkX >= 0 && checkX < gridSizeX)
+        {
+            if (checkY >= 0 && checkY < gridSizeY)
+            {
+                NeighborList.Add(grid[checkX, checkY]);
+            }
+        }
+        checkX = a_NeighborNode.gridX;
+        checkY = a_NeighborNode.gridY - 1;
+        if (checkX >= 0 && checkX < gridSizeX)
+        {
+            if (checkY >= 0 && checkY < gridSizeY)
+            {
+                NeighborList.Add(grid[checkX, checkY]);
+            }
+        }
+
+        return NeighborList;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y));
+        if (grid != null)
+        {
+            foreach(Node n in grid)
+            {
+                if(n.IsWall)
+                {
+                    Gizmos.color = Color.red;
+                }
+                else
+                {
+                    Gizmos.color = Color.yellow;
+                }
+
+                if (Path != null)
+                {
+                    if (Path.Contains(n))
+                    {
+                        Gizmos.color = Color.blue;
+                    }
+                }
+
+                Gizmos.DrawWireCube(n.Position, new Vector3(1*(nodeDiameter - Distance), 1* (nodeDiameter - Distance)));
+            }
+        }
+    }
+    
+
+
+}
---
+++ Namcap/Assets/Scripts/Node.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public int gridX;//x in array
+    public int gridY;//y in array
+    public bool IsWall;// Is it a wall?
+    public Vector3 Position;//world position of node
+    public Node Parent;//Previous node for sake of A*
+    public int gCost;//cost to move to next square
+    public int hCost;//distance from goal
+    public int FCost { get { return gCost + hCost; } }// it calculates f cost
+    public Node(bool a_IsWall,Vector3 a_Pos, int a_gridX, int a_gridY)//do i really need to comment what this does?
+    {
+        IsWall = a_IsWall;
+        Position = a_Pos;
+        gridX = a_gridX;
+        gridY = a_gridY;
+    }
+}
