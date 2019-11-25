d6e9c237515a534a579f945c242db2c360ba6490
CMay75 <46727122+CMay75@users.noreply.github.com>
2019-11-24 22:50:00 -0500

    Added a Basic Main Menu
    Added a basic main menu with a button that loads the first level.

--- Namcap/Assets/Scripts/DoorController.cs
+++ Namcap/Assets/Scripts/DoorController.cs
@@ -8,18 +8,11 @@ public class DoorController : MonoBehaviour
     private SpriteRenderer door;
     private Sprite exit;
 
-    Scene currentScene;
-    string currSceneName;
-
     // Start is called before the first frame update
     private void Start()
     {
         door = GetComponent<SpriteRenderer>();
         exit = Resources.Load<Sprite>("Exit");
-
-        currentScene = SceneManager.GetActiveScene();
-        currSceneName = currentScene.name;
-
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -28,14 +21,7 @@ public class DoorController : MonoBehaviour
         {
             if (GameObject.FindWithTag("Key") == null)
             {
-                if (currSceneName == "1-Summer")
-                {
-                    SceneManager.LoadScene("2-Fall", LoadSceneMode.Single);
-                }
-                else if (currSceneName == "2-Fall")
-                {
-                    SceneManager.LoadScene("3-Winter", LoadSceneMode.Single);
-                }
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
         }
     }
