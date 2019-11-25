cffcb996a6d85cf0eab09acd6d4a3c974f2ec590
CMay75 <46727122+CMay75@users.noreply.github.com>
2019-11-24 20:58:45 -0500

    Changed SpriteChange Script to DoorController
    Changed the SpriteChange script to DoorController and placed the code for changing levels in it; Fixed the issue with the door sprite not changing properly.

---
+++ Namcap/Assets/Scripts/DoorController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorController : MonoBehaviour
+{
+    private SpriteRenderer door;
+    private Sprite exit;
+
+    Scene currentScene;
+    string currSceneName;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        door = GetComponent<SpriteRenderer>();
+        exit = Resources.Load<Sprite>("Exit");
+
+        currentScene = SceneManager.GetActiveScene();
+        currSceneName = currentScene.name;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (GameObject.FindWithTag("Key") == null)
+            {
+                if (currSceneName == "1-Summer")
+                {
+                    SceneManager.LoadScene("2-Fall", LoadSceneMode.Single);
+                }
+                else if (currSceneName == "2-Fall")
+                {
+                    SceneManager.LoadScene("3-Winter", LoadSceneMode.Single);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(GameObject.FindWithTag("Key") == null)
+        {
+            door.sprite = exit;
+        }
+    }
+}
