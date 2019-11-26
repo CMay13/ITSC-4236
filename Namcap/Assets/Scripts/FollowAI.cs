a69f7e60ec500c89f386e3761778522b77c71c74
jhenry33 <jhenry33@uncc.edu>
2019-11-26 00:33:12 -0500

    FollowAI "finish"

---
+++ Namcap/Assets/Scripts/FollowAI.cs
@@ -0,0 +1,45 @@
+﻿use using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAI : MonoBehaviour
+{
+    public Grid grid;
+    public float EnemySpeed;
+    List<Node> follow;
+    Coroutine MoveIE;
+    private void Awake()
+    {
+       Grid grid = GetComponentInChildren<Grid>();
+    }
+    private void Update()
+    {
+        follow = grid.Path;
+        StartCoroutine(movement());
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+    IEnumerator movement()
+    {
+        for (int i = 0; i < follow.Count; i++)
+        {
+            MoveIE = StartCoroutine(Moving(i));
+            yield return MoveIE;
+        }
+    }
+
+    IEnumerator Moving(int currentPosition)
+    {
+        while (transform.position != follow[currentPosition].Position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, follow[currentPosition].Position, EnemySpeed * Time.deltaTime);
+            yield return null;
+        }
+
+    }
+}
