2a25c5b58a0a26221ad10b5a1b978f8f6db4bc73
Vincent Popp <vpopp@uncc.edu>
2019-11-25 22:31:52 -0500

    Added level interludes.

---
+++ Namcap/Assets/Scripts/Interlude.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Interlude : MonoBehaviour
+{
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
